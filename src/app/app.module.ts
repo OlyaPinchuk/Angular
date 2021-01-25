8787244c203c260ec6e3908a70c4c31eee523603
OlyaPinchuk <olha5959@gmail.com>
2021-01-25 19:38:09 +0200

    Add changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([{
+      path: 'link/home', component: HomeComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
