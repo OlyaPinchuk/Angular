3e57488fa6ebe29f3b8e9a8a0959a0240a451dea
OlyaPinchuk <olha5959@gmail.com>
2021-02-01 03:34:31 +0200

    Add changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'this is the page of my angular app';
 
-  checkInput(myInput: HTMLInputElement): void {
-    console.log(myInput);
+  // checkInput(myInput: HTMLInputElement): void {
+  //   console.log(myInput);
+  // }
+// ===============================================================================
+
+  // user = {name: 'Nick', age: 233};
+  //
+  // checkForm(myForm: NgForm): void {
+  //   console.log(myForm);
+  // }
+// ===============================================================================
+
+  name = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  age = new FormControl('', Validators.required);
+
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age,
+  });
+
+  submitForm(): void {
+    console.log(this.myForm);
   }
+
 }
