61bd23d7cf04ffdb14ab1619e105a3ca14ad8e30
OlyaPinchuk <olha5959@gmail.com>
2021-02-01 18:52:58 +0200

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    CarsComponent,
-    CarComponent
   ],
   imports: [
     BrowserModule
