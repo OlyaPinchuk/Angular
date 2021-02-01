f513ee73cd84732a4cec70199d160f9927a3a7be
OlyaPinchuk <olha5959@gmail.com>
2021-02-01 19:31:39 +0200

    Add changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+// import {UserModule} from './aa/user.module';
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
+    ])
+    // UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+
+const routes: Routes = [
+  {path: '', component: UsersComponent, children: [
+      {path: ':id', component: UserComponent}
+]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
